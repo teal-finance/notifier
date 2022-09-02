4487c68d07de78aa0cbda8223c9efb6e8969dd2e
Oliver <0uep@hessling.fr>
2022-09-02 15:46:50 +0200

    🔒️ Prevent log injection

--- logger/logger.go
+++ logger/logger.go
@@ -3,6 +3,8 @@ package logger
 
 import (
 	"log"
+	"strings"
+	"unicode/utf8"
 )
 
 // Notifier is an empty struct.
@@ -15,6 +17,31 @@ func NewNotifier() Notifier {
 
 // Notify prints the messages to the logs.
 func (n Notifier) Notify(msg string) error {
-	log.Print("INF LoggerNotifier: ", msg)
+	log.Print("INF LoggerNotifier: ", sanitize(msg))
 	return nil
+}
+
+// The code points in the surrogate range are not valid for UTF-8.
+const (
+	surrogateMin = 0xD800
+	surrogateMax = 0xDFFF
+)
+
+// sanitize replaces control codes by the tofu symbol
+// and invalid UTF-8 codes by the replacement character.
+// sanitize can be used to prevent log injection.
+//
+// Inspired from:
+// - https://wikiless.org/wiki/Replacement_character#Replacement_character
+// - https://graphicdesign.stackexchange.com/q/108297
+func sanitize(str string) string {
+	return strings.Map(func(r rune) rune {
+		switch {
+		case r < 32, r == 127: // The .notdef character is often represented by the empty box (tofu)
+			return '􏿮' // to indicate a valid but not rendered character.
+		case surrogateMin <= r && r <= surrogateMax, utf8.MaxRune < r:
+			return '�' // The replacement character U+FFFD indicates an invalid UTF-8 character.
+		}
+		return r
+	}, str)
 }
