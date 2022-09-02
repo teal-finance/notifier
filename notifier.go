2e3d7fb26259488a3491f5fcd23f9feccadd76e9
Oliver <0uep@hessling.fr>
2022-09-02 15:47:46 +0200

    ✨ Introduce a global New() selecting the type depending on URL pattern

--- notifier.go
+++ notifier.go
@@ -1,6 +1,24 @@
 package notifier
 
+import (
+	"log"
+
+	"github.com/teal-finance/notifier/logger"
+	"github.com/teal-finance/notifier/mattermost"
+)
+
 // Notifier interface for sending messages.
 type Notifier interface {
 	Notify(message string) error
+}
+
+// New selects the Notifier type depending on the endpoint pattern.
+func New(endpoint string) Notifier {
+	switch endpoint {
+	case "":
+		log.Print("INF empty URL => use the logger Notifier")
+		return logger.NewNotifier()
+	default:
+		return mattermost.NewNotifier(endpoint)
+	}
 }
